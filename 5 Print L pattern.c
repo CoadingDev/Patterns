f1ff410371d4e897f9da8bf0ae12711ea4630ab3
Tushar Mishra <137509314+CoadingDev@users.noreply.github.com>
2026-01-23 07:35:40 +0530

    Add files via upload

---
+++ 5 Print L pattern.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (i = 1; i <= 4; i++) {
+        printf("*\n");
+    }
+
+    
+    for (i = 1; i <= 5; i++) {
+        printf("*");
+    }
+
+    return 0;
+}
+
+//Single Loop
+#include <stdio.h>
+int main(){
+    for(int i=1;i<=5;i++){
+        print("*");
+    }
+}
